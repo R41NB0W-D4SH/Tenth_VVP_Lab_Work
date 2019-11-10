dbd8143cb4829a71d73941289481d10e77901739
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-10 18:38:45 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+    int n, sum;
+
+	sum = 0;
+    cout << "Введите число N" << endl;
+    cin >> n;
+
+    cout << "Сейчас сумма равна " << sum << endl;
+
+    for(int i = 1; i <= 2 * n - 1; i += 2) 
+	{
+        sum += i;
+        cout << "Теперь сумма равна " << sum << endl;
+    }
+
+    cout << "Квадрат N равен " << sum << endl;
+
+	system("pause");
+    return 0;
+}
