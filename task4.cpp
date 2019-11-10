64c5212aa82c595d59fd09793337613b3232d73e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-10 18:53:41 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+    int i, n; 
+	double a, sum = 0;
+
+    cout << "Введите N, затем A. " << endl;
+    cin >> n >> a;
+
+	if (n > 0)
+	{
+		for(i = 0; i <= n; i++)
+		{
+			sum = sum + pow(a, i);
+		}
+		cout << "Сумма равна " << sum << endl;
+	}
+	else
+	{
+	cout << "N должен быть больше нуля. Попробуйте еще раз." << endl;
+	}
+
+	system("pause");
+    return 0;
+}
