4c9a1383f863300ed124648c220532f9fd3172e6
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-10 18:29:00 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale (LC_ALL, "Russian");
+	int N;
+	double i, j, result; 
+
+	result = 1;
+	j = 1.1;
+	cout << "Введите количество сомножителей." << endl;
+	cin >> N;
+
+	if (N > 0)
+	{
+		for (i = 0; i < N; i ++, j += 0.1)
+		{
+			result = result * j;
+		}
+		cout << "Произведение равно " << result << endl;
+	}
+	else
+	{
+		cout << "Количество множителей должно быть больше нуля. Попробуйте еще раз." << endl;
+	}
+
+	system("pause");
+	return 0;
+}
